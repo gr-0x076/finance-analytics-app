164a28431a8f83d9a3e7ef1616f5cb13b24ebf6e
Mohith GR <grmohith06@gmail.com>
2026-06-19 21:28:29 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,18 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-    System.out.print("$ ");
-    String input = scanner.nextLine();
+            System.out.print("$ ");
+            String input = scanner.nextLine();
 
-    if (input.equals("exit")) {
-        break;
-    }
-
-    System.out.println(input + ": command not found");
-}
+            if (input.equals("exit")) {
+                break;
+            } 
+            else if (input.startsWith("echo ")) {
+                System.out.println(input.substring(5));
+            } 
+            else {
+                System.out.println(input + ": command not found");
+            }
+        }
     }
 }
